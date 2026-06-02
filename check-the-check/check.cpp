88747435c5bb2e55d46a602408d745ebceee3a60
thomas-cad <t.cadegros@outlook.fr>
2026-06-02 14:32:11 +0200

    check the check

--- check-the-check/check.cpp
+++ check-the-check/check.cpp
@@ -1,5 +1,28 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstring>
 
 int main(){
-    
+    char buff[1000];
+    int q, p;
+    int total=0;
+    while (1){
+        scanf(" %[^\n]",buff);
+        int rt = strcmp(buff, "TOTAL");
+        if (rt==0) break;
+
+        scanf("%d %d", &q, &p);
+
+        total+=q*p;
+    }
+
+    int price;
+    scanf("%d", &price);
+
+    if (price<=total){
+        printf("PAY\n");
+    }else{
+        printf("PROTEST\n");
+    }
+
+    return 0;
 }
